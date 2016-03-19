908c0fe8cd78151a0be079d44d738f43a38cc715
unknown <at15@outlook.com>
2016-03-19 10:06:53 +0800

    Fix mysql no intereaction install
    have to look into the source for install script and get the name

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -8,8 +8,12 @@ echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" | sudo tee /etc/ap
 sudo apt-get update
 
 echo "Set default mysql root password as vagrant"
+# FIXME: mysql 5.7.11 is installing mysql community server
+# https://github.com/mysql/mysql-server/blob/b4104b21520be032400b768cea09a867068be49d/packaging/deb-trusty/mysql-community-server.postinst
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password vagrant'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password vagrant'
+sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/root-pass password vagrant'
+sudo debconf-set-selections <<< 'mysql-community-server mysql-community-server/re-root-pass password vagrant'
 
 echo "Installing mysql"
-sudo apt-get install -y mysql-server mysql-client
+sudo apt-get install -y --force-yes mysql-server mysql-client
--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -5,9 +5,10 @@
 echo "installing the latest stable version"
 
 sudo apt-key add nginx_signing.key
+sudo apt-key update
 
 echo "deb http://ppa.launchpad.net/nginx/stable/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list > /dev/null
 echo "deb-src http://ppa.launchpad.net/nginx/stable/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y nginx
+sudo apt-get install -y --force-yes nginx
