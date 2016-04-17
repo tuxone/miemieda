7dc141d76bcb39bebb22df7be4944da2b17aa33d
at15 <at15@outlook.com>
2016-04-17 22:58:21 +0800

    Fix #5 change fpm sock position in config
    - add sqlite extension for php7.0

--- scripts/php.sh
+++ scripts/php.sh
@@ -7,7 +7,7 @@ print_green "Installing PHP"
 echo "Install php and extensions"
 sudo apt-get install -y php7.0-fpm \
 php7.0-cli php7.0-curl php7.0-gd \
-php7.0-intl php7.0-mysql \
+php7.0-intl php7.0-mysql php7.0-sqlite3 \
 php7.0-mcrypt php7.0-json php7.0-xml php7.0-mbstring php7.0-zip
 
 # php7.0-xdebug is not supported
