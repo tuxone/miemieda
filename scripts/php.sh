a056eae5327249a825ccfe0bf94bce69e2d516c9
unknown <at15@outlook.com>
2016-05-16 00:39:11 +0800

    For #11 add xdebug and redis extension for PHP7

--- scripts/php.sh
+++ scripts/php.sh
@@ -8,10 +8,8 @@ echo "Install php and extensions"
 sudo apt-get install -y php7.0-fpm \
 php7.0-cli php7.0-curl php7.0-gd \
 php7.0-intl php7.0-mysql php7.0-sqlite3 \
-php7.0-mcrypt php7.0-json php7.0-xml php7.0-mbstring php7.0-zip
-
-# php7.0-xdebug is not supported
-
+php7.0-mcrypt php7.0-json php7.0-xml php7.0-mbstring php7.0-zip \
+php7.0-xdebug php7.0-redis
     
 # PHP Error Reporting Config
 echo "enable all the error report for develop"
